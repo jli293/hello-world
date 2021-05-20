b40d043379b57b231a86f55771e30f9db85305cb
slbicky <slbicky@seas.upenn.edu>
2021-05-20 10:40:45 -0400

    Added print: Sarah's comments

--- hello_world.py
+++ hello_world.py
@@ -8,6 +8,7 @@ def main():
  print ("My name is Efua. Welcome to PennChats")
  print("Hello it's SPARC not SPRAC")
  print("Audra's comment")
+ print("Sarah's comments")
 
 if __name__ == '__main__':
     main()
