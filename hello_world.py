27d8d3b0b82c61badcd72c66814ab6ba4075e943
jli293 <84085953+jli293@users.noreply.github.com>
2021-08-28 12:26:59 -0400

    changes to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -9,12 +9,7 @@ def hello():
     prints "hello world from flaskland"
     """
 
-    return "<p>Hello World from Flaskland!</p>"
-
- print ("My name is Efua. Welcome to PennChats")
- print("Hello it's SPARC not SPRAC")
- print("Audra's comment")
- print("Sarah's comments")
+    return "<p>Hello World from Jimmy!</p>"
 
 if __name__ == '__main__':
     app.run()
